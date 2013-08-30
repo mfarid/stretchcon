5f5bd9f2c757c021c3c3a082c8c6f148ec8880f3
Gábor Török <gabor.torok@prezi.com>
2013-08-30 12:18:02 +0200

    Format title

--- docpad.js
+++ docpad.js
@@ -8,7 +8,7 @@ module.exports = {
 
     // Conference info
     conf: {
-        name: "Stretch conference. First you grew. Now you lead.",
+        name: "Stretch Conference — First you grew. Now you lead.",
         description: "Join us at Stretch in Budapest this December and get inspired by stories from leads and founders of great organisations as they talk about the challenges of leadership and management.",
       date: "December 5-6, 2013",
 //      price: "$100",
