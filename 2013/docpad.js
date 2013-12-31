4f670d681a5b977c112373cf35e0b47129dbc4d7
Gábor Török <gabor.torok@prezi.com>
2013-12-31 12:54:24 +0100

    Add photos section

--- 2013/docpad.js
+++ 2013/docpad.js
@@ -33,6 +33,7 @@ module.exports = {
         // you can also change order here and it will reflect on page
         sections: [
             'summary',
+            'photos',
             'about',
             'schedule',
             'speakers',
